76043304724832fd30215a4d61f580e6bad1e7e2
skabusaeed1 <shaikhabusaeed1@gmail.com>
2023-07-27 23:09:30 +0530

    half project

--- frontend/src/components/Details/Details.jsx
+++ frontend/src/components/Details/Details.jsx
@@ -18,24 +18,31 @@ import Navbar from '../Navbar/Navbar'
 
 const Details = () => {
 
-  const {data}=useSelector((store)=>{
-    return store.GetReducers
+  const data=useSelector((store)=>{
+    return store.GetReducers.detailsPageData
   })
+  console.log(data)
 
   return (
       <div>
       <Navbar/>
         <Box className="details">
       <Box className="img-div">
-        <img src="https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?cs=srgb&dl=pexels-binyamin-mellish-1396122.jpg&fm=jpg" alt="img" />
+        <img src={data.images} alt="img" />
       </Box>
       <Box className="box">
        <VStack>
-        <h2>Name:- Mannat House</h2>
+        <h2>Name:- {data.name}</h2>
         <Center><h5 style={{fontSize:"1.5rem",marginTop:"20px"}}>Description</h5></Center>
-        <p>Oceanfront Resort offers a blissful getaway with its stunning beachfront location. Guests can enjoy direct beach access, unwind with rejuvenating spa treatments, dine at the on-site restaurant, and stay active at the fitness centre.</p>
-        <h3>Location:- Pune</h3>
-        <h3>Price:- 30L</h3>
-        <button>Add to Card</button>
+        <p>{data.description}</p>
+        <h3>Location:- {data.location}</h3>
+        <h3>Price:- {data.price}L</h3>
+        <Link to='/card'><button>Add to Card</button></Link>
        </VStack>
-      
+      </Box>
+    </Box>
+      </div>
+  );
+};
+
+export default Details;
